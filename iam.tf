34aff34cd1a743722f175174346a06cd2f2285c5
Brent Rogers <brentarogers@gmail.com>
2024-08-06 13:46:42 -0400

    fix arn

--- iam.tf
+++ iam.tf
@@ -88,5 +88,5 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_access" {
 
 resource "aws_iam_role_policy_attachment" "ssm_managed" {
   role = aws_iam_role.necesse_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMManagedInstanceCore"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
